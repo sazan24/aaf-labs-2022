959cb85056bcaed27146e3ef38f2a930654f30a4
sazan24 <86345471+sazan24@users.noreply.github.com>
2022-11-04 12:08:31 +0200

    Update parser.py
    У файлі parser.py було виправлено деякі помилки, які були вказані у зауваженнях, а саме:
    1) Multi-line введення команд;
    2) Назви таблиць у команді CREATE можна вводити без лапок;
    3) Реалізація рядків у команді INSERT тепер підтримує основні ASCII символи.

--- sakhnii_fb-01_prykhodko_fb-01/parser.py
+++ sakhnii_fb-01_prykhodko_fb-01/parser.py
@@ -1,16 +1,28 @@
 # Практично повноцінна реалізація інтерфейсу командного рядка (взаємодія із користувачем) і парсеру команд.
 import re
+import os
 
-pattern_create = re.compile("^[Cc][Rr][Ee][Aa][Tt][Ee]\s+[a-zA-Z][a-zA-Z0-9_]*\s+\(\s*\"[a-zA-Z][a-zA-Z0-9_]*\"(\s*,\s*\"[a-zA-Z][a-zA-Z0-9_]*\")*\)+;$")
-pattern_insert = re.compile("^[Ii][Nn][Ss][Ee][Rr][Tt](\s+[Ii][Nn][Tt][Oo])?\s+[a-zA-Z][a-zA-Z0-9_]*\s+\(\s*\"[a-zA-Z][a-zA-Z0-9_]*\"(\s*,\s*\"[a-zA-Z][a-zA-Z0-9_]*\")*\)+;$")
-pattern_select = re.compile("^[Ss][Ee][Ll][Ee][Cc][Tt]\s+[Ff][Rr][Oo][Mm]\s+[a-zA-Z][a-zA-Z0-9_]*\s*;$")
-pattern_exit = re.compile("^[Ee][Xx][Ii][Tt]\s*$")
-patterns = [pattern_create, pattern_insert, pattern_select, pattern_exit]
+pattern_create = re.compile("^[Cc][Rr][Ee][Aa][Tt][Ee]\s+[a-zA-Z][a-zA-Z0-9_]*\s+\(\s*[a-zA-Z][a-zA-Z0-9_]*(\s*,\s*[a-zA-Z][a-zA-Z0-9_]*)*\s*\)\s*;\s*$")
+pattern_insert = re.compile("^[Ii][Nn][Ss][Ee][Rr][Tt](\s+[Ii][Nn][Tt][Oo])?\s+[a-zA-Z][a-zA-Z0-9_]*\s+\(\s*\".+\"(\s*,\s*\".+\")*\s*\)\s*;\s*$")
+pattern_select = re.compile("^[Ss][Ee][Ll][Ee][Cc][Tt]\s+[Ff][Rr][Oo][Mm]\s+[a-zA-Z][a-zA-Z0-9_]*\s*;\s*$")
+pattern_exit = re.compile("^[Ee][Xx][Ii][Tt]\s*;\s*$")
+pattern_clear = re.compile("^[Cc][Ll][Ee][Aa][Rr]\s*;\s*$")
+patterns = [pattern_create, pattern_insert, pattern_select, pattern_exit, pattern_clear]
 print("◙ Welcome to our program, which was created by students of group FB-01: Prykhodko Ihor and Sakhnii Nazar 🙂\n\n"
       "♦ To test the command line interface, please ENTER basic database query commands ↓ (To finish type \"EXIT\")")
 
 while True:
-    command = input(">>> ")
+    command = ""
+    i = 0
+    while True:
+        if i == 0:
+           line = input(">>> ")
+        else:
+            line = input("... ")
+        i += 1
+        command += (line + "\n")
+        if ";" in line:
+            break
     result = -1
     for i in range(len(patterns)):
         if patterns[i].match(command):
@@ -22,27 +34,33 @@ while True:
                   "Try to make query in next format → CREATE {table_name} ({column_name_№1} [,..., {column_name_№n}]);")
         elif bool(re.match("^[Ii][Nn][Ss][Ee][Rr][Tt]", command)):
             print("Incorrect \"INSERT\" syntax. \n"
-                  "Try to make query in next format → INSERT [INTO] {table_name} ({value_№1} [,..., {value_№n}]);")
+                  "Try to make query in next format → INSERT [INTO] {table_name} ('{value_№1}' [,..., '{value_№n}']);")
         elif bool(re.match("^[Ss][Ee][Ll][Ee][Cc][Tt]", command)):
             print("Incorrect \"SELECT\" syntax. \n"
                   "Try to make query in next format → SELECT FROM {table_name};")
         elif bool(re.match("^[Ee][Xx][Ii][Tt]", command)):
             print("Incorrect \"EXIT\" syntax. \n"
-                  "Please! Try to write this simple command correctly ;-)")
+                  "Format of \"EXIT\" command: EXIT;")
+        elif bool(re.match("^[Cc][Ll][Ee][Aa][Rr]", command)):
+            print("Incorrect \"CLEAR\" syntax. \n"
+                  "Format of \"CLEAR\" command: CLEAR;")
         else:
             print("Unknown command :-(")
     else:
         if result == 0:
-            tokens_create = re.findall("\"?[a-zA-Z][a-zA-Z0-9_]*\"?", command)  # Maybe will be useful in the future to have arguments with ""
+            tokens_create = re.findall("[a-zA-Z][a-zA-Z0-9_]*", command)  
             print("Tokens of \"CREATE\" command:")
             print(tokens_create)
-        if result == 1:
-            tokens_insert = re.findall("[a-zA-Z][a-zA-Z0-9_]*", command)
+        elif result == 1:
+            tokens_insert = re.findall("(\".+?\")|([a-zA-Z][a-zA-Z0-9_]*)", command)
             print("Tokens of \"INSERT\" command:")
             print(tokens_insert)
-        if result == 2:
+        elif result == 2:
             tokens_select = re.findall("[a-zA-Z][a-zA-Z0-9_]*", command)
             print("Tokens of \"SELECT\" command:")
             print(tokens_select)
-        if result == 3:
+        elif result == 3:
             break
+        elif result == 4:
+            os.system("cmd /c cls")
+       
